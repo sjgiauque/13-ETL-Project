e4e0b1b091b7eb0b010851be73fa1326aed80bc5
Brian Walker <bmw5140@gmail.com>
2020-02-01 13:21:17 -0500

    adding project files to github

---
+++ project_2_sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE renewable_energy_db (
+year INT PRIMARY KEY,
+fossil_fuels_qbtu DECIMAL,
+nuclear_qbtu DECIMAL,
+renewable_qbtu DECIMAL,
+total_energy_production_qbtu DECIMAL,
+electricity_emissions DECIMAL,
+us_population INT	
+);
